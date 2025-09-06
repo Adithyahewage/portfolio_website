a69beee322ac135f863f18f930a81a1281519c44
Adithya Hewage <adithyahewage69@gmail.com>
2025-09-06 23:08:02 +0530

    created blogs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Blogs from "./components/Blogs";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects />
+      <Blogs />
     </>
   );
 }
